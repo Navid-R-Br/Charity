16d72d1b4dd02461fd10d9c2a6ebf86f600b806b
TUF <ad@>
2026-05-29 14:52:42 +0330

    Change2 commit

--- about_us/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import about_us
-
-urlpatterns = [
-    path('', about_us),
-]
--- about_us/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import get_user_model
-from django.shortcuts import render
-
-
-def about_us(request):
-    context = {
-        'members': get_user_model().objects.all()
-    }
-    return render(request, 'about_us.html', context)
